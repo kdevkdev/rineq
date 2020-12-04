f90558fab88aba39c714650b94f2bdedf9cc3caa
kaspar meili <kaspar.meili@ispm.unibe.ch>
2020-12-04 08:34:13 +0100

    fix check() non-ascii warning, fix argument name in as.numeric

--- R/as.double.hci.R
+++ R/as.double.hci.R
@@ -1,6 +1,6 @@
 as.double.hci <-
-  function(object, ...) {
-    if (!any(class(object) == 'hci')) stop("Object is not of class hci")
+  function(x, ...) {
+    if (!any(class(x) == 'hci')) stop("Object is not of class hci")
 
-    object$concentration_index
+    x$concentration_index
   }
